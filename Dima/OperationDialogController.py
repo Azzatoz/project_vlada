296833cf5e1c51585221e35091e48d45f945734f
Azzatoz <83659233+Azzatoz@users.noreply.github.com>
2024-04-01 17:39:31 +0300

    # Изменил код под MySQL

--- Dima/OperationDialogController.py
+++ Dima/OperationDialogController.py
@@ -1,7 +1,4 @@
-import sqlite3
-from PyQt5 import QtCore, QtGui, QtWidgets
-from support_file import SupportClass
-from Dima.AddDialog import Ui_AddDialog
+import mysql.connector
 from support_file import show_notification
 
 
@@ -38,10 +35,9 @@ class OperationDialogController:
     # TODO: добавить в support_file
     def get_selected_items(self):
         selected_items = []
-
         # Проходим по всем строкам таблицы
         for row in range(self.table_widget.rowCount()):
-            item = self.table_widget.item(row, 3)
+            item = self.table_widget.item(row, 4)
 
             # Проверяем, заполнен ли четвертый столбец (количество на...)
             if item is not None and item.text():
@@ -56,12 +52,11 @@ class OperationDialogController:
                         row_data.append("")
 
                 selected_items.append(row_data)
-
         return selected_items
 
     def get_worker_id(self, worker_name):
         try:
-            self.cursor.execute("SELECT id FROM Worker WHERE name=?", (worker_name,))
+            self.cursor.execute("SELECT id FROM Worker WHERE name=%s", (worker_name,))
             worker_row = self.cursor.fetchone()
             if worker_row:
                 return worker_row[0]
@@ -74,17 +69,16 @@ class OperationDialogController:
 
     def get_client_or_warehouse_id(self, selected_destination):
         try:
-            destination_name = selected_destination
             if self.operation_type == "Продать":
-                self.cursor.execute("SELECT id FROM Client WHERE name=?", (destination_name,))
+                self.cursor.execute("SELECT id FROM Client WHERE name=%s", (selected_destination,))
             else:
-                self.cursor.execute("SELECT id FROM Warehouse WHERE name=?", (destination_name,))
+                self.cursor.execute("SELECT id FROM Warehouse WHERE name=%s", (selected_destination,))
             client_or_warehouse_row = self.cursor.fetchone()
             if client_or_warehouse_row:
                 return client_or_warehouse_row[0]
             else:
                 self.info_line.setText(
-                    f"{'' if self.operation_type == 'Продать' else 'Склад '}с именем '{destination_name}' не найден.")
+                    f"{'' if self.operation_type == 'Продать' else 'Склад '}с именем '{selected_destination}' не найден.")
                 return None
         except sqlite3.Error as e:
             self.info_line.setText(
@@ -92,36 +86,42 @@ class OperationDialogController:
                     e))
             return None
 
+    def get_warehouse_id(self, warehouse_name):
+        warehouse = warehouse_name
+        self.cursor.execute("SELECT id FROM Warehouse WHERE name=%s", (warehouse,))
+        warehouse_row = self.cursor.fetchone()
+
+        return warehouse_row[0]
+
     def insert_operation_data(self, operation_type, client_or_warehouse_id, worker_id, current_time,
                               additional_characteristics, selected_items):
         try:
             # Вставляем данные операции в таблицу
             self.cursor.execute(
                 "INSERT INTO Operation (type, client_id, worker_id, time, additional_characteristics) "
-                "VALUES (?, ?, ?, ?, ?)",
+                "VALUES (%s, %s, %s, %s, %s)",
                 (operation_type, client_or_warehouse_id, worker_id, current_time, additional_characteristics))
 
             # Получаем id только что вставленной операции
             operation_id = self.cursor.lastrowid
-
-            # Проходим по всем выбранным товарам и вставляем данные в таблицу Operation_product
             for selected_row_data in selected_items:
                 product_id = selected_row_data[0]  # ID товара на складе
-                quantity_to_operate = int(selected_row_data[3])  # Количество для операции
-
+                quantity = int(selected_row_data[3])
+                quantity_to_operate = int(selected_row_data[4])  # Количество для операции
+                warehouse_id = self.get_warehouse_id(selected_row_data[6])
                 # Проверяем, что количество для операции меньше или равно общему количеству товара на складе
-                if quantity_to_operate <= int(selected_row_data[2]):  # Общее количество товара
+                if quantity_to_operate <= quantity:  # Общее количество товара
                     self.cursor.execute(
-                        "INSERT INTO Operation_product (operation_id, product_id, warehouse_id, quantity, condition) "
-                        "VALUES (?, ?, ?, ?, ?)",
-                        (operation_id, product_id, client_or_warehouse_id, quantity_to_operate, operation_type))
+                        "INSERT INTO Operation_product (operation_id, product_id, warehouse_id, quantity, condition_type)"
+                        "VALUES (%s, %s, %s, %s, %s)",
+                        (operation_id, product_id, warehouse_id, quantity_to_operate, operation_type))
                 else:
                     self.info_line.setText(
                         "Ошибка: Количество для операции превышает общее количество товара на складе.")
                     return
 
             self.connection.commit()
-        except sqlite3.Error as e:
+        except mysql.connector.Error as e:
             self.info_line.setText("Ошибка при записи данных операции в базу данных: " + str(e))
 
     def update_product_information(self, selected_items, selected_warehouse_id=None):
@@ -129,19 +129,20 @@ class OperationDialogController:
             if selected_items:
                 for selected_row_data in selected_items:
                     item_id = selected_row_data[0]  # ID товара на складе
-                    quantity_in_warehouse = int(selected_row_data[2])
-                    quantity_to_operate = int(selected_row_data[3])
+                    quantity_in_warehouse = int(selected_row_data[3])
+                    print(selected_items)
+                    quantity_to_operate = int(selected_row_data[4])
                     new_quantity_for_warehouse = quantity_in_warehouse - quantity_to_operate
 
                     # Обновляем информацию о складе, если указан конкретный склад
                     if selected_warehouse_id:
-                        self.cursor.execute("UPDATE Current_product SET warehouse_id=? WHERE id=?",
+                        self.cursor.execute("UPDATE Current_product SET warehouse_id=%s WHERE id=%s",
                                             (selected_warehouse_id, item_id))
 
-                    # меняем изначальное количество товара
-                        self.cursor.execute("UPDATE Current_product SET quantity=? WHERE id=?",
+                        # меняем изначальное количество товара
+                        self.cursor.execute("UPDATE Current_product SET quantity=%s WHERE id=%s",
                                             (new_quantity_for_warehouse, item_id))
 
                 self.connection.commit()
-        except sqlite3.Error as e:
+        except mysql.connector.Error as e:
             self.info_line.setText("Ошибка при обновлении информации о товаре: " + str(e))
